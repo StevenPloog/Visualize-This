a8f8bbd054670d887d81945bd89e757da6fb2857
StevenPloog <emailstevenp@gmail.com>
2015-07-01 21:48:34 -0500

    Updated flag visualizer to be more American

--- js/v/Flag.js
+++ js/v/Flag.js
@@ -7,11 +7,13 @@ var Flag = function(canvas, analyser) {
 }
 
 Flag.prototype.draw = function() {
-	var canvas = this.canvas;
+    var canvas = this.canvas;
     var drawContext = this.drawContext;
     var freqDomain = new Float32Array(this.analyser.frequencyBinCount);
 
     drawContext.clearRect(0, 0, canvas.width, canvas.height);
+    drawContext.fillStyle="black";
+    drawContext.fillRect(0, 0, canvas.width, canvas.height);
     this.analyser.getFloatFrequencyData(freqDomain);
 
     var maxFreq         = 900;
@@ -23,11 +25,11 @@ Flag.prototype.draw = function() {
     var samplesPerStar  = Math.floor(maxFreq / numStars)
     
     var flagLeft        = .125 * canvas.width;
-    var flagWidth 	    = .750 * canvas.width;
+    var flagWidth       = .750 * canvas.width;
     var flagTop         = .125 * canvas.height;
     var flagHeight      = .750 * canvas.height;
     var stripeHeight    = flagHeight / numStripes;
-    var starRadius		= 25;
+    var starRadius      = 25;
 
     var starRectWidth   = .4 * flagWidth;
     var starRectHeight   = 7 * stripeHeight;
@@ -35,7 +37,7 @@ Flag.prototype.draw = function() {
     var starYSpace      = starRectHeight / numStarRows;
 
     for (var i = 0; i < numStripes; i++) {
-    	var value = 0;
+        var value = 0;
 
         for (var x = 0; x < samplesPerStripe; x++) {
             value += freqDomain[i*samplesPerStripe + x];
@@ -46,27 +48,47 @@ Flag.prototype.draw = function() {
         value = nonNegative(value);
         value = value / Visualizer.decibelRange;
 
-        drawContext.fillStyle = 'hsl(' + i*30 + ', 100%, 50%)';
-    	drawContext.fillRect(flagLeft, flagTop+i*stripeHeight, flagWidth * value, stripeHeight);
+        var hue = 45*value;
+        if (i % 2 == 1) {
+            //hue *= .5;
+            hue += 340;
+        } else {
+            hue += 200;
+        }
+
+        drawContext.fillStyle = 'hsl(' + hue + ', 100%, ' + Math.floor(100*value) + '%)';
+
+        //drawContext.fillStyle = 'hsl(' + i*30 + ', 100%, 50%)';
+        drawContext.fillRect(flagLeft, flagTop+i*stripeHeight, flagWidth, stripeHeight);
     }
 
-    drawContext.fillStyle = 'hsl(0, 0%, 0%)';
-    drawContext.fillRect(flagLeft, flagTop, starRectWidth, starRectHeight);
+    drawContext.fillStyle = 'hsl(0, 0%, 100%)';
+    drawContext.fillRect(flagLeft-1, flagTop-1, starRectWidth+1, starRectHeight+1);
 
     for (var x = 0; x < numStarCols; x++) {
         for (var y = 0; y < numStarRows; y++) {
             var value = 0;
+            var startFreqIndex = x + y*numStarCols;
+            startFreqIndex *= samplesPerStar;
 
             for (var i = 0; i < samplesPerStar; i++) {
-                value += freqDomain[(x+1)*(y+1)*samplesPerStar + i];
+                value += freqDomain[startFreqIndex + i];
                 value -= this.analyser.minDecibels;
-                value -= weight(Visualizer.frequencyPerBin * (x*y*samplesPerStar + i));
+                value -= weight(Visualizer.frequencyPerBin * (startFreqIndex + i));
             }
             value /= samplesPerStar;
-            value = nonNegative(value);
+            //value = nonNegative(value);
             value = value / Visualizer.decibelRange;
 
-            drawContext.fillStyle = 'hsl(' + 360*value + ', 100%, 50%)';
+            var hue = 45*value;
+            if (value < .5) {
+                //hue *= .5;
+                hue += 340;
+            } else {
+                hue += 200;
+            }
+
+            drawContext.fillStyle = 'hsl(' + hue + ', 100%, ' + Math.floor(40+50*value) + '%)';
             drawContext.beginPath();
             drawContext.arc(flagLeft+.5*starXSpace + starXSpace*x,
                             flagTop +.5*starYSpace + starYSpace*y,
