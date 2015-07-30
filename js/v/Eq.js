057e8b3f987c5ae79b8f80c36751c1201a2772e4
Steven <emailstevenp@gmail.com>
2015-07-29 21:34:23 -0400

    Update Eq and restore Spectrum to the proper code

--- js/v/Eq.js
+++ js/v/Eq.js
@@ -1,10 +1,10 @@
 var Eq = function(canvas, analyser) {
 
-	this.canvas = canvas;
-	this.analyser = analyser;
+    this.canvas = canvas;
+    this.analyser = analyser;
     this.drawContext = this.canvas.getContext('2d');
 
-    this.targetFrequencies = [63, 160, 400, 1000, 2500, 6250, 16000];
+    this.targetFrequencies = [40, 80, 160, 400, 1000, 2500, 6250, 16000];
 
 }
 
@@ -16,32 +16,33 @@ Eq.prototype.draw = function () {
     this.analyser.getFloatFrequencyData(freqDomain);
 
     for (var i = 0; i < this.targetFrequencies.length; i++) {
-    	var mid = this.targetFrequencies[i];
-    	var start = mid - mid/2;
-    	var end = mid + mid/2;
+        var mid = this.targetFrequencies[i];
+        var start = mid - mid/2;
+        var end = mid + mid/2;
 
-    	var midIndex = Math.floor(mid / Visualizer.frequencyPerBin);
-    	var startIndex = Math.floor(start / Visualizer.frequencyPerBin);
-    	var endIndex = Math.floor(end / Visualizer.frequencyPerBin);
+        var midIndex = Math.floor(mid / Visualizer.frequencyPerBin);
+        var startIndex = Math.floor(start / Visualizer.frequencyPerBin);
+        var endIndex = Math.floor(end / Visualizer.frequencyPerBin);
 
-    	if (endIndex > 1024) endIndex = 1024;
+        if (endIndex > 1024) endIndex = 1024;
 
-    	var value = 0;
+        var value = 0;
 
-    	for (var x = startIndex; x < endIndex; x++) {
-    		value += freqDomain[x] * (1 - Math.abs(midIndex - x) / midIndex);
-    		value -= this.analyser.minDecibels;
-    	}
+        for (var x = startIndex; x < endIndex; x++) {
+            value += freqDomain[x] * (1 - Math.abs(midIndex - x) / midIndex);
+            value -= this.analyser.minDecibels * (1 - Math.abs(midIndex - x) / midIndex);
+            value -= weight(mid) * (1 - Math.abs(midIndex - x) / midIndex);
+        }
 
-    	value = value / (endIndex - startIndex);
-    	value = nonNegative(value);
-    	value /= Visualizer.decibelRange;
+        value = value / (endIndex - startIndex);
+        value = nonNegative(value);
+        value /= Visualizer.decibelRange;
 
-    	var barWidth = this.canvas.width / this.targetFrequencies.length;
-    	var barHeight = value * this.canvas.height;
+        var barWidth = this.canvas.width / this.targetFrequencies.length;
+        var barHeight = value * this.canvas.height;
 
 
-    	this.drawContext.fillStyle = 'hsl(' + 50*i + ', 100%, 50%)';
-        this.drawContext.fillRect(i * barWidth, 250, barWidth, 10+barHeight);
+        this.drawContext.fillStyle = 'hsl(' + 50*i + ', 100%, 50%)';
+        this.drawContext.fillRect(i * barWidth, this.canvas.height, barWidth, -barHeight);
     }
 }
--- js/v/Spectrum.js
+++ js/v/Spectrum.js
@@ -1,48 +1,38 @@
-var Eq = function(canvas, analyser) {
+var Spectrum = function(canvas, analyser) {
 
-    this.canvas = canvas;
+	this.canvas = canvas;
     this.analyser = analyser;
     this.drawContext = this.canvas.getContext('2d');
 
-    this.targetFrequencies = [63, 160, 400, 1000, 2500, 6250, 16000];
-
 }
 
-Eq.prototype.draw = function () {
+Spectrum.prototype.draw = function () {
 
     var freqDomain = new Float32Array(this.analyser.frequencyBinCount);
 
     this.drawContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.analyser.getFloatFrequencyData(freqDomain);
 
-    for (var i = 0; i < this.targetFrequencies.length; i++) {
-        var mid = this.targetFrequencies[i];
-        var start = mid - mid/2;
-        var end = mid + mid/2;
-
-        var midIndex = Math.floor(mid / Visualizer.frequencyPerBin);
-        var startIndex = Math.floor(start / Visualizer.frequencyPerBin);
-        var endIndex = Math.floor(end / Visualizer.frequencyPerBin);
-
-        if (endIndex > 1024) endIndex = 1024;
-
+    var maxFreq = 790;
+    var slicesPerBar = 10;
+    for (var i = 1; i < maxFreq; i += slicesPerBar) {
         var value = 0;
 
-        for (var x = startIndex; x < endIndex; x++) {
-            value += freqDomain[x] * (1 - Math.abs(midIndex - x) / midIndex);
-            value -= this.analyser.minDecibels * (1 - Math.abs(midIndex - x) / midIndex);
-            value -= weight(mid) * (1 - Math.abs(midIndex - x) / midIndex);
+        for (var x = 0; x < slicesPerBar; x++) {
+            value += freqDomain[i+x];
+            value -= this.analyser.minDecibels;
+            value -= weight(Visualizer.frequencyPerBin * (i+x));
         }
-
-        value = value / (endIndex - startIndex);
+        value /= slicesPerBar;
         value = nonNegative(value);
-        value /= Visualizer.decibelRange;
 
-        var barWidth = this.canvas.width / this.targetFrequencies.length;
-        var barHeight = value * this.canvas.height;
-
-
-        this.drawContext.fillStyle = 'hsl(' + 50*i + ', 100%, 50%)';
-        this.drawContext.fillRect(i * barWidth, this.canvas.height, barWidth, -barHeight);
+        var percent = value / Visualizer.decibelRange;
+        var height = this.canvas.height * percent;
+        var offset = this.canvas.height - height - 1;
+        var barWidth =  this.canvas.width/maxFreq;
+        var hue = i/maxFreq * 360;
+    
+        this.drawContext.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+        this.drawContext.fillRect(i * barWidth, offset, slicesPerBar*barWidth, height);
     }
 }
